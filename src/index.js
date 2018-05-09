cff4af05b25483baf6ef6d0b1e6dd4570af10bc2
Sophie <sophie4ngo@gmail.com>
2018-05-09 00:15:46 -0700

    Add bulma

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import '../node_modules/bulma/css/bulma.css';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
