8bb94c518ad69ee384576d1ec3e60471acf673a8
Sophie <sophie4ngo@gmail.com>
2018-05-20 23:39:17 -0700

    Fix ` characters

--- src/components/login.js
+++ src/components/login.js
@@ -27,7 +27,7 @@ class Login extends Component {
       errors: null
     })
 
-    axios.post('${process.env.REACT_APP_BASE_URL}/login', data)
+    axios.post(`${process.env.REACT_APP_BASE_URL}/login`, data)
       .then(response => {
           console.log(response);
           if (response.status === 200 && response.data.auth) {
